254fd3b2eb6313bee0dbbb8b0e02c341a9daf263
JakubMagdziak <114910578+JakubMagdziak@users.noreply.github.com>
2024-05-06 14:34:04 +0200

    zad 5

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -2,7 +2,7 @@ package edu.kis.vh.nursery;
 
 public class FIFORhymer extends defaultCountingOutRhymer {
 
-    public defaultCountingOutRhymer temp = new defaultCountingOutRhymer();
+    public defaultcountingoutrhymer temp = new defaultCountingOutRhymer();
     
     @Override
     public int countOut() {
